60e628476e5fb04b0c6717b5d7e0d8ba49a5198e
blankunderscore900 <nicholas_bollong@outlook.com>
2021-06-14 00:49:03 -0300

    Working with Unity.Ink

--- Lost_Of_Words/Assets/Scripts/TextInk.cs
+++ Lost_Of_Words/Assets/Scripts/TextInk.cs
@@ -2,28 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ink.Runtime;
+using UnityEngine.UI;
+using TMPro;
 
 public class TextInk : MonoBehaviour
 {
     public TextAsset inkJSON;
     private Story story;
 
+    public TextMeshProUGUI textPrefab;
+    public Button buttonPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
         story = new Story(inkJSON.text);
-        Debug.Log(loadStoryChunk());
 
-        for(int i = 0; i < story.currentChoices.Count; i++)
+        refreshUI();
+    }
+
+    void refreshUI()
+    {
+        eraseUI();
+
+        TextMeshProUGUI storyText = Instantiate(textPrefab) as TextMeshProUGUI;
+        storyText.text = loadStoryChunk();
+        storyText.transform.SetParent(this.transform, false);
+
+        foreach (Choice choice in story.currentChoices)
         {
-            Debug.Log(story.currentChoices[i].text);
+            Button choiceButton = Instantiate(buttonPrefab) as Button;
+            TextMeshProUGUI choiceText = buttonPrefab.GetComponentInChildren<TextMeshProUGUI>();
+            choiceText.text = choice.text;
+            choiceButton.transform.SetParent(this.transform, false);
+
+            choiceButton.onClick.AddListener(delegate
+            {
+                chooseStoryChoice(choice);
+            });
         }
+    }
 
-        //Debug.Log(story.currentChoices);
+    void eraseUI()
+    {
+        for(int i = 0; i < this.transform.childCount; i++)
+        {
+            Destroy(this.transform.GetChild(i).gameObject);
+        }
+    }
 
-        story.ChooseChoiceIndex(0);
-        Debug.Log(loadStoryChunk());
-        Debug.Log(loadStoryChunk());
+    void chooseStoryChoice(Choice choice)
+    {
+        story.ChooseChoiceIndex(choice.index);
+        refreshUI();
     }
 
     // Update is called once per frame
